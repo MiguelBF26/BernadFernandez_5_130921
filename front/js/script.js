c7f27f6231c61885c5fbd720a152fc09ad997808
miguel.bernad@modiscloud.net <miguel.mbf06@gmail.com>
2022-01-31 16:36:00 +0100

    Fin du projet

--- front/js/script.js
+++ front/js/script.js
@@ -1,152 +1,48 @@
+fillSection();
 
-/*
-async function getproducts() {
-    // Penser à remplacer par les données récupérées dans le json
-    const products = [
-        {
-          "colors": ["Blue", "White", "Black"],
-          "_id": "107fb5b75607497b96722bda5b504926",
-          "name": "Kanap Sinopé",
-          "price": 1849,
-          "imageUrl": "kanap01.jpeg",
-          "description": "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-          "altTxt": "Photo d'un canapé bleu, deux places"
-        },
-        {
-          "colors": ["Black/Yellow", "Black/Red"],
-          "_id": "415b7cacb65d43b2b5c1ff70f3393ad1",
-          "name": "Kanap Cyllène",
-          "price": 4499,
-          "imageUrl": "kanap02.jpeg",
-          "description": "Morbi nec erat aliquam, sagittis urna non, laoreet justo. Etiam sit amet interdum diam, at accumsan lectus.",
-          "altTxt": "Photo d'un canapé jaune et noir, quattre places"
-        },
-        {
-          "colors": ["Green", "Red", "Orange"],
-          "_id": "055743915a544fde83cfdfc904935ee7",
-          "name": "Kanap Calycé",
-          "price": 3199,
-          "imageUrl": "kanap03.jpeg",
-          "description": "Pellentesque fermentum arcu venenatis ex sagittis accumsan. Vivamus lacinia fermentum tortor.Mauris imperdiet tellus ante.",
-          "altTxt": "Photo d'un canapé d'angle, vert, trois places"
-        },
-        {
-          "colors": ["Pink", "White"],
-          "_id": "a557292fe5814ea2b15c6ef4bd73ed83",
-          "name": "Kanap Autonoé",
-          "price": 1499,
-          "imageUrl": "kanap04.jpeg",
-          "description": "Donec mattis nisl tortor, nec blandit sapien fermentum at. Proin hendrerit efficitur fringilla. Lorem ipsum dolor sit amet.",
-          "altTxt": "Photo d'un canapé rose, une à deux place"
-        },
-        {
-          "colors": ["Grey", "Purple", "Blue"],
-          "_id": "8906dfda133f4c20a9d0e34f18adcf06",
-          "name": "Kanap Eurydomé",
-          "price": 2249,
-          "imageUrl": "kanap05.jpeg",
-          "description": "Ut laoreet vulputate neque in commodo. Suspendisse maximus quis erat in sagittis. Donec hendrerit purus at congue aliquam.",
-          "altTxt": "Photo d'un canapé gris, trois places"
-        },
-        {
-          "colors": ["Grey", "Navy"],
-          "_id": "77711f0e466b4ddf953f677d30b0efc9",
-          "name": "Kanap Hélicé",
-          "price": 999,
-          "imageUrl": "kanap06.jpeg",
-          "description": "Curabitur vel augue sit amet arcu aliquet interdum. Integer vel quam mi. Morbi nec vehicula mi, sit amet vestibulum.",
-          "altTxt": "Photo d'un canapé gris, deux places"
-        },
-        {
-          "colors": ["Red", "Silver"],
-          "_id": "034707184e8e4eefb46400b5a3774b5f",
-          "name": "Kanap Thyoné",
-          "price": 1999,
-          "imageUrl": "kanap07.jpeg",
-          "description": "EMauris imperdiet tellus ante, sit amet pretium turpis molestie eu. Vestibulum et egestas eros. Vestibulum non lacus orci.",
-          "altTxt": "Photo d'un canapé rouge, deux places"
-        },
-        {
-          "colors": ["Pink", "Brown", "Yellow", "White"],
-          "_id": "a6ec5b49bd164d7fbe10f37b6363f9fb",
-          "name": "Kanap orthosie",
-          "price": 3999,
-          "imageUrl": "kanap08.jpeg",
-          "description": "Mauris molestie laoreet finibus. Aenean scelerisque convallis lacus at dapibus. Morbi imperdiet enim metus rhoncus.",
-          "altTxt": "Photo d'un canapé rose, trois places"
+// Récupération des articles de l'API
+async function getArticles() {
+    var articlesCatch = await fetch("http://localhost:3000/api/products")
+    return await articlesCatch.json();
+}
+
+    // Répartition des données de l'API dans le DOM
+async function fillSection() {
+    var result = await getArticles ()
+    .then(function (resultatAPI){
+        const articles = resultatAPI;
+        console.table(articles);
+        for (let article in articles) {
+
+            // Insertion de l'élément "a"
+            let productLink = document.createElement("a");
+            document.querySelector(".items").appendChild(productLink);
+            productLink.href = `product.html?id=${resultatAPI[article]._id}`;
+
+            // Insertion de l'élément "article"
+            let productArticle = document.createElement("article");
+            productLink.appendChild(productArticle);
+
+            // Insertion de l'image
+            let productImg = document.createElement("img");
+            productArticle.appendChild(productImg);
+            productImg.src = resultatAPI[article].imageUrl;
+            productImg.alt = resultatAPI[article].altTxt;
+
+            // Insertion du titre "h3"
+            let productName = document.createElement("h3");
+            productArticle.appendChild(productName);
+            productName.classList.add("productName");
+            productName.innerHTML = resultatAPI[article].name;
+
+            // Insertion de la description "p"
+            let productDescription = document.createElement("p");
+            productArticle.appendChild(productDescription);
+            productDescription.classList.add("productName");
+            productDescription.innerHTML = resultatAPI[article].description;
         }
-      ];
-    // et bien retourner le tableau photographers seulement une fois
-    return ({
-        products: [...products, ...products, ...products]})
-}
-*/
-async function displayData(products) {
-    
-    console.log(products);
-    
-    products.forEach((product) => {
-        console.log(product);
+    })
+    .catch (function(error){
+        return error;
     });
-};
-
-const getProducts = async function () {
-  let response = await fetch('http://localhost:3000/api/products');
-  let data = await response.json();
-  return data;
 }
-
-async function init() {
-    // Récupère les datas des produit
-    const products = await getProducts();
-    displayData(products);
-};
-
-init();
-
-
-
-/*async function getPhotographers() {
-    // Penser à remplacer par les données récupérées dans le json
-    const photographers = [
-        {
-            "name": "Ma data test",
-            "id": 1,
-            "city": "Paris",
-            "country": "France",
-            "tagline": "Ceci est ma data test",
-            "price": 400,
-            "portrait": "account.png"
-        },
-        {
-            "name": "Autre data test",
-            "id": 2,
-            "city": "Londres",
-            "country": "UK",
-            "tagline": "Ceci est ma data test 2",
-            "price": 500,
-            "portrait": "account.png"
-        },
-    ]
-    // et bien retourner le tableau photographers seulement une fois
-    return ({
-        photographers: [...photographers, ...photographers, ...photographers]})
-}
-
-async function displayData(photographers) {
-    const photographersSection = document.querySelector(".photographer_section");
-
-    photographers.forEach((photographer) => {
-        const photographerModel = photographerFactory(photographer);
-        const userCardDOM = photographerModel.getUserCardDOM();
-        photographersSection.appendChild(userCardDOM);
-    });
-};
-
-async function init() {
-    // Récupère les datas des photographes
-    const { photographers } = await getPhotographers();
-    displayData(photographers);
-};
-
-init();*/
